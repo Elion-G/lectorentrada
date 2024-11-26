3683847a7f590fcf355671a136fee6d2c381a527
Elion-G <eliassantiagonzales1909@gmail.com>
2024-11-26 11:39:32 -0300

    storage

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
