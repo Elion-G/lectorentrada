70448ecc6cb90e378ead13b1997abaae8162a41e
Elion-G <eliassantiagonzales1909@gmail.com>
2024-12-02 09:20:59 -0300

    prueba nueva

--- resources/js/qrcode.js
+++ resources/js/qrcode.js
@@ -38,28 +38,48 @@ window.addEventListener('DOMContentLoaded', () => {
     //     }
     // }
 
-    function onScanSuccess(decodeText, decodeResult) {
+    async function sendRequest(cin) {
+        try {
+            const data = await new Promise((resolve, reject) => {
+                $.ajax({
+                    url: "http://lectorentrada.test/buscar-funcionario",
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Cookie": "XSRF-TOKEN=eyJpdiI6Ii9ndUJTTkZVWmttV2drUVB4LzFtS2c9PSIsInZhbHVlIjoiSjZKZktqdElMQ0ZWQ2xWS01NL0h0blBrOXBGby9MQjB4V0kwYXMxRlZVOXI2cS9tdmVqNXRWaFhjR09QbmNrcTg3R0JVV1VxK3FxeDRwWVhzZFN5aXNZNUpPOEg0RWJFVG5oeExnb21DcFpBaWsrRGNiRStWOHY0djJvOGF0SmEiLCJtYWMiOiIwMWI4ZWE1MGMwMGU0ZTE2Nzg2ZWE1ZDU2YTUzYzQ4YzNjNDI5ZTNlOTJmY2U3ZmZjYWRjYmRhMDE2N2RiYjg1IiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6Im1ab1BxR2FBWEt0VzBuWWhqTUxQUWc9PSIsInZhbHVlIjoiR0htaVVUdEVRQ0V4bkdyN1BZMWVvN3hNNHNkNm5OMU9PL3pJdXRBWFJjckVUUm9jYnVkSU1aUWVIbDFxWnBIWkJubEIzZmpWRHhWY1h1Q1B0ek1EV0Q2WlE1bTNBSEtlaEFPeFJNYUJ1NjhOclpwdkJXL3M4dnFDbk9pQXBCTG4iLCJtYWMiOiI0OTE5YzM5OWEyNmRlOGQ1OGJmM2Y0NmUzZWFkMjZmMjM0MzYwZWM0NTdmNjM4MzkyYjBiMmY4OWI2OWEyYTgxIiwidGFnIjoiIn0%3D"
+                    },
+                    data: JSON.stringify({ "cin" : cin }),
+                    success: resolve,
+                    error: (xhr, status, error) => reject(new Error(`Error: ${status}, ${error}`))
+                });
+            });
+            return data;
+        } catch (error) {
+            console.error('Error en la solicitud:', error);
+            alert('Hubo un error al procesar la solicitud.');
+            return null;
+        }
+    }
+
+    async function onScanSuccess(decodeText, decodeResult) {
         if (decodeText !== lastResult) {
             lastResult = decodeText;
             cinInput.value = decodeText;
 
-            // Realiza la solicitud POST usando async/await
-            var settings = {
-            "url": "http://lectorentrada.test/buscar-funcionario",
-            "method": "GET",
-            "timeout": 0,
-            "headers": {
-                "Content-Type": "application/json",
-                "Cookie": "XSRF-TOKEN=eyJpdiI6Ii9ndUJTTkZVWmttV2drUVB4LzFtS2c9PSIsInZhbHVlIjoiSjZKZktqdElMQ0ZWQ2xWS01NL0h0blBrOXBGby9MQjB4V0kwYXMxRlZVOXI2cS9tdmVqNXRWaFhjR09QbmNrcTg3R0JVV1VxK3FxeDRwWVhzZFN5aXNZNUpPOEg0RWJFVG5oeExnb21DcFpBaWsrRGNiRStWOHY0djJvOGF0SmEiLCJtYWMiOiIwMWI4ZWE1MGMwMGU0ZTE2Nzg2ZWE1ZDU2YTUzYzQ4YzNjNDI5ZTNlOTJmY2U3ZmZjYWRjYmRhMDE2N2RiYjg1IiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6Im1ab1BxR2FBWEt0VzBuWWhqTUxQUWc9PSIsInZhbHVlIjoiR0htaVVUdEVRQ0V4bkdyN1BZMWVvN3hNNHNkNm5OMU9PL3pJdXRBWFJjckVUUm9jYnVkSU1aUWVIbDFxWnBIWkJubEIzZmpWRHhWY1h1Q1B0ek1EV0Q2WlE1bTNBSEtlaEFPeFJNYUJ1NjhOclpwdkJXL3M4dnFDbk9pQXBCTG4iLCJtYWMiOiI0OTE5YzM5OWEyNmRlOGQ1OGJmM2Y0NmUzZWFkMjZmMjM0MzYwZWM0NTdmNjM4MzkyYjBiMmY4OWI2OWEyYTgxIiwidGFnIjoiIn0%3D"
-            },
-            "data": JSON.stringify({
-                "cin": "5266710"
-            }),
-            };
-            
-            $.ajax(settings).done(function (response) {
-                alert(response);
-            });
+            try {
+                const response = await sendRequest(cinInput.value);
+    
+                if (response) {
+                    if (response.success) {
+                        showInfoView(response.data);
+                    } else {
+                        alert('No se encontró la persona.');
+                    }
+                }
+            } catch (error) {
+                console.error('Error procesando la solicitud:', error);
+                alert('Hubo un problema al buscar al funcionario.');
+            }
         }
     }
 
