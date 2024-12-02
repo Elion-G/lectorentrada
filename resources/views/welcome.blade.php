6b201c4eb01072202f36304013fa538bf85845b0
Elion-G <eliassantiagonzales1909@gmail.com>
2024-12-02 11:34:04 -0300

    mt-5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,7 @@
             </div>
         </div>
 
-        <div id="info-ingresado" class="mt-5 container flex-column justify-content-center align-items-center vw-100 d-none">
+        <div id="info-ingresado" class="mt-2 container flex-column justify-content-center align-items-center vw-100 d-none">
             <h1 class="h1-block text-danger">
                 <i class="bi bi-ban"></i>
             </h1>
